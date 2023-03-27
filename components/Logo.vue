3db9628a13994c703c412f2c8a18f67d8ff25bd0
Thomas J. Vu <thomasjvu@gmail.com>
2023-03-27 19:01:48 -0400

    Update page layout and script

---
+++ components/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img src="../assets/img/pointblanks_400px.png" />
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scope>
+img {
+    width: 50px;
+    border-radius: 50%;
+    border: 1px solid white;
+}
+</style>
