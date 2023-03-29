7bb8850066e28c879d3ab01c38e7a26574adb81f
Thomas Vu <thomasjvu@gmail.com>
2023-03-29 01:13:00 -0400

    Update cors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@ export default defineNuxtConfig({
     extends: "@nuxt-themes/docus",
     routeRules: {
         // Add cors headers
-        "/api/": { cors: true, headers: { 'access-control-allow-methods': '* '} }
+        "/*": { cors: true, headers: { 'access-control-allow-methods': '* '} }
     }
 });
